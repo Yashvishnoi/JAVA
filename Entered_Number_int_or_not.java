ac82b8b1da271c4a4b52fb19f885c9b1792e1820
Yash Vishnoi <jivishnoi@gmail.com>
2021-01-25 21:41:01 +0530

    Wheather a entered number is integer or not

---
+++ Entered_Number_int_or_not.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Entered_Number_int_or_not {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the number \n");
+		Scanner sc = new Scanner (System.in);
+		System.out.println(sc.hasNextInt());
+	}
+
+}
