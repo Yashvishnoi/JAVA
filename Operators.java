57cb6af60b5f7fbd871d023c0d5cebb0e613747c
Yash Vishnoi <jivishnoi@gmail.com>
2021-01-30 15:33:45 +0530

    Operators

---
+++ Operators.java
@@ -0,0 +1,14 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int b=9;
+		b+=3;
+		System.out.println(b);
+		b*=3;
+		System.out.println(b);
+		System.out.println(6<=b); // Comparison operator
+		System.out.println(6<b && b<8); // Logical operator
+	}
+
+}
