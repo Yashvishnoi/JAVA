3d2176075d8454af68120485ebc6a8ab69d234a4
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-01 11:18:15 +0530

    Static and non-static member

---
+++ Strings.java
@@ -0,0 +1,10 @@
+
+public class Strings {
+
+	public static void main(String[] args) {
+		String name = new String("Harry");
+		System.out.println(name);
+
+	}
+
+}
---
+++ static_and_nonStatic.java
@@ -0,0 +1,28 @@
+
+class Student {
+	int SapId;
+	String name;
+	static int RollNo = 0;
+	static String college = "upes";
+// Constructor
+
+	Student (int s ,String n) {
+		SapId= s;
+		name = n;
+		RollNo++;
+	}
+	
+	
+}
+public class static_and_nonStatic {
+
+	public static void main(String[] args) {
+		Student s1 = new Student(500077690,"Vijay");
+		System.out.println(s1.RollNo);
+		Student s2 = new Student(500077611,"Aman");  
+
+		System.out.println(s2.RollNo);  
+		
+	}
+
+}
