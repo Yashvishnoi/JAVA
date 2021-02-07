d262dc8806b08f2ab929f4ad1973e13254f02ff3
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-07 14:25:01 +0530

    Strings

--- Strings.java
+++ Strings.java
@@ -1,10 +1,22 @@
-
-public class Strings {
+import java.util.Scanner;
+class Strings{
 
 	public static void main(String[] args) {
-		String name = new String("Harry");
-		System.out.println(name);
-
+	//	String name = new String("Harry");
+		/*String name= "Harry";
+		System.out.print("The name is: "); // It will not print in next line
+		System.out.println(name); // It will print in same line
+		int a = 10;
+		float b= 10.5f;
+		String s = "Yash ";
+		char c = 'E';
+		System.out.printf("The value of int is %d, float is %f, char is %c and string is %s  ",a,b,c,s);
+		*/Scanner sc = new Scanner(System.in);
+		//String st = sc.next(); // It will take only one word
+		String st = sc.nextLine(); // It will take whole entered line
+		
+		System.out.println(st);
+		
 	}
 
 }
