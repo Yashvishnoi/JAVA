dc81120f649b21e6d21bba66786060b4f9e63566
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-08 14:08:37 +0530

    String Functions

--- String_Method.java
+++ String_Method.java
@@ -13,6 +13,13 @@ public class String_Method {
 		String nonTrimmedString = "   HarryBoi  ";
 		System.out.println("The Untrimmed String "+ nonTrimmedString);
 		System.out.println("The trimed string is " +nonTrimmedString.trim());// It will remove the Spaces of before and after the String
- 	}
+		System.out.println("Substring(3) "+name.substring(3)); // it will print from 3rd position till end in case of harry it will print ry.
+		System.out.println("Substring(2,4) "+name.substring(2, 4)); // start string included and end string excluded.
+		System.out.println("Returns after replacing r with p = " + name.replace('r', 'p')); // It will replace all r with p
+		System.out.println("Returns true or false in this case = "+name.startsWith("Ha") ); // It will check whether string start with ha or not
+		System.out.println("Returns true or false in this case = "+ name.endsWith("ky") ); // It will check whether string end with ry or not
+		System.out.println("Returns Character at given index " + name.charAt(2)); // Returns character at that position
+		System.out.println("Returns index of the given String "+ name.indexOf("ar"));
+	}
 
 }
