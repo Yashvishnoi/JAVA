3129adf9fdc645e6a396c13c68ad93c35e200500
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-11 23:56:10 +0530

    Structure

---
+++ PracticeSwitchCase2.java
@@ -0,0 +1,11 @@
+// Calculate income for paid by an employee to the goverment as per the slabs mentioned below:
+import java.util.Scanner;
+public class PracticeSwitchCase2  {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		int slab = sc.nextInt();
+		
+	}
+
+}
