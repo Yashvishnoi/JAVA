9d6892e70362334bbd125604a62c0506b2782032
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-12 14:38:02 +0530

    Update the 8-21

--- PracticeSwitchCase2.java
+++ PracticeSwitchCase2.java
@@ -3,9 +3,21 @@ import java.util.Scanner;
 public class PracticeSwitchCase2  {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); 
-		int slab = sc.nextInt();
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the salary in lakhs ");
+		double slab = sc.nextDouble();
+		if(slab>2.5 && slab<5.0) {
+			System.out.println("Pay tax 5% ");
+		}
+		else if(slab>5.0 && slab<10.0) {
+			System.out.println("Pay 20% tax");
+		}
+		else if(slab>10.0){
+			System.out.println("Pay 30% tax");
+		}
+		else {
+			System.out.println("No tax chaos !! ");
+		}
 	}
 
 }
