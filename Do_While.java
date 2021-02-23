aa2e14d09f6864d8668ce79404e033a5406a433f
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-23 11:33:51 +0530

    do while

--- Do_While.java
+++ Do_While.java
@@ -1,9 +1,21 @@
-
+// Print n natural number using do-while loop
+import java.util.Scanner;
 public class Do_While {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the n value ");
+		
+		int n=sc.nextInt();
+		int b=0;
+		do {
+			System.out.println(b);
+			b++;
+		}while(b<n);
+		
+		System.out.println("Enter the n value ");
+		
+		
 	}
 
 }
