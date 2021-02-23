72833ab9d1ffa7387eb0ad2dc9b6089b6d4b0f05
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-23 12:30:29 +0530

    reverse order

--- For_Loops.java
+++ For_Loops.java
@@ -1,13 +1,19 @@
+// Natural number in increasing and decreasing order
 import java.util.Scanner;
 
 public class For_Loops {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value of n:");
+		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(); 
-		for(int i=0;i<n;i++) {
+		System.out.println("\nNatural Number in increasing order");
+		for(int i=0;i<=n;i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("\nNatural number in decreasing order ");
+		for(int i=n;i>=0;i--) {
 			System.out.println(i);
 		}
 	}
