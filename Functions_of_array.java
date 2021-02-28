733af7218bd8a61a016772f9ab21345cb282b3fb
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-28 23:50:58 +0530

    to find array length function

---
+++ Functions_of_array.java
@@ -0,0 +1,10 @@
+
+public class Functions_of_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] marks = {5,7,8,9,10};
+		System.out.println(marks.length);
+	}
+
+}
