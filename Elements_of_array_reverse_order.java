430e413683d2114121dfa1596e48da56f8f1badb
Yash Vishnoi <jivishnoi@gmail.com>
2021-02-28 23:49:03 +0530

    Deleted comments[D[D[D[D[D[D[D[D[D[D[

--- Elements_of_array_reverse_order.java
+++ Elements_of_array_reverse_order.java
@@ -5,7 +5,6 @@ public class Elements_of_array_reverse_order {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int [] array = new int [5];
-		//int [] array = {5,4,3,2,1};
 		int i;
 		for(i=0;i<5;i++) {
 			System.out.println("Enter the value of array at index "+ i);
