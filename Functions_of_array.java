b117070386c51a61cc36a3cfeb1016466264be6a
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-01 00:21:13 +0530

    Added the loo[

--- Functions_of_array.java
+++ Functions_of_array.java
@@ -4,10 +4,12 @@ public class Functions_of_array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] marks = {5,7,8,9,10};
-		System.out.println(marks.length);
+		System.out.println("The length of marks is "+marks.length);
 		
 		String [] Student = {"Araj", "Anrul","Harshu","Sagel"};
-		System.out.println(Student[0]);
+		for(int i = 0;i<Student.length;i++) {
+		System.out.println(Student[i]);
+		}
 	}
 
 }
