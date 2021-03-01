524fd32b80ed337a96df270d714a86644b8fc036
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-01 11:51:21 +0530

    2d array sum

--- PracticeSet_Of_Arrays.java
+++ PracticeSet_Of_Arrays.java
@@ -51,7 +51,7 @@ public class PracticeSet_Of_Arrays {
 
 
 // 3. Calculate the average marks from array using for each loop	
-	System.out.println("Problem 3");
+/*	System.out.println("Problem 3");
 	float [] Physic = {10.5f,20.5f,60.8f, 69.69f , 41.5f};
 	float avg;
 	float sum=0;
@@ -60,6 +60,35 @@ public class PracticeSet_Of_Arrays {
 	}
 	avg = sum /  (Physic.length);
 	System.out.println("The average of Physic marsks ="+avg );
+	
+*/
+		
+		
+		
+// 4. Adding 2d matrices of size [2][3];
+	
+int [][] mat1 = {{1, 2, 3},
+            	{4, 5, 6}};
+int [][] mat2 = {{2, 6, 13},
+            	{3, 7, 1}};
+int [][] result = {{0, 0, 0},
+              	{0, 0, 0}};
+
+for (int i=0;i<mat1.length;i++){ // row number of times
+for (int j=0;j<mat1[i].length;j++) { // column number of time
+   System.out.format(" Setting value for i=%d and j=%d\n", i, j);
+   result[i][j] = mat1[i][j] + mat2[i][j];
+}
+}
+
+// Printing the elements of a 2-D Array
+for (int i=0;i<mat1.length;i++){ // row number of times
+for (int j=0;j<mat1[i].length;j++) { // column number of time
+   System.out.print(result[i][j] + " ");
+   result[i][j] = mat1[i][j] + mat2[i][j];
+}
+System.out.println(""); // Prints a new line
+}
 	}
 	
 }
