c65ddeac90617840582eb6a391203f8c322c0f2d
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-01 11:40:47 +0530

    Average of a array using for each loop

--- PracticeSet_Of_Arrays.java
+++ PracticeSet_Of_Arrays.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class PracticeSet_Of_Arrays {
 
 	public static void main(String[] args) {
-		System.out.println("Problem 1");
 		Scanner sc = new Scanner(System.in);
+
 // 1. Array of 5 floats and calculate their sum	
+/*		System.out.println("Problem 1");
 		float [] array = new float[5];
 		int i;
 		System.out.println("Enter the array values");
@@ -17,7 +18,7 @@ public class PracticeSet_Of_Arrays {
 		/* This is done using for loop
 		for(i=0;i<array.length;i++) {
 			sum=sum+array[i];
-		}*/
+		}
 		
 		// This is done using for each loop
 		for(float element:array) {
@@ -25,11 +26,11 @@ public class PracticeSet_Of_Arrays {
 		}
 		
 		System.out.println("Sum ="+sum);
-
+*/
 		
 		
 // 2. Wheather an element is present in array or not
-//		System.out.println("Problem 2");
+/*		System.out.println("Problem 2");
 		float [] Arry = {10.5f,20.5f,60.8f, 69.69f , 41.5f};
 		float number = 60.8f;
 		boolean isInArray = false;
@@ -45,7 +46,21 @@ public class PracticeSet_Of_Arrays {
 		else {
 			System.out.println("Number is not present in Array");
 		}
-				
-		}
+*/				
+
+
+
+// 3. Calculate the average marks from array using for each loop	
+	System.out.println("Problem 3");
+	float [] Physic = {10.5f,20.5f,60.8f, 69.69f , 41.5f};
+	float avg;
+	float sum=0;
+	for(float element:Physic) {
+		sum=sum+element;
 	}
+	avg = sum /  (Physic.length);
+	System.out.println("The average of Physic marsks ="+avg );
+	}
+	
+}
 
