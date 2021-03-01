54f67641fac2623ff3aea611c04491624f8f4c7a
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-01 14:21:44 +0530

    Sorted Array or not

--- PracticeSet_Of_Arrays.java
+++ PracticeSet_Of_Arrays.java
@@ -66,29 +66,47 @@ public class PracticeSet_Of_Arrays {
 		
 		
 // 4. Adding 2d matrices of size [2][3];
-	
-int [][] mat1 = {{1, 2, 3},
+/*	
+	int [][] mat1 = {{1, 2, 3},
             	{4, 5, 6}};
-int [][] mat2 = {{2, 6, 13},
+    int [][] mat2 = {{2, 6, 13},
             	{3, 7, 1}};
-int [][] result = {{0, 0, 0},
+    int [][] result = {{0, 0, 0},
               	{0, 0, 0}};
 
-for (int i=0;i<mat1.length;i++){ // row number of times
-for (int j=0;j<mat1[i].length;j++) { // column number of time
-   System.out.format(" Setting value for i=%d and j=%d\n", i, j);
-   result[i][j] = mat1[i][j] + mat2[i][j];
-}
-}
+    for (int i=0;i<mat1.length;i++){ // row number of times
+    	for (int j=0;j<mat1[i].length;j++) { // column number of time
+    		System.out.format(" Setting value for i=%d and j=%d\n", i, j);
+    		result[i][j] = mat1[i][j] + mat2[i][j];
+    	}
+    }
 
-// Printing the elements of a 2-D Array
-for (int i=0;i<mat1.length;i++){ // row number of times
-for (int j=0;j<mat1[i].length;j++) { // column number of time
-   System.out.print(result[i][j] + " ");
-   result[i][j] = mat1[i][j] + mat2[i][j];
-}
-System.out.println(""); // Prints a new line
-}
+    // Printing the elements of a 2-D Array
+    for (int i=0;i<mat1.length;i++){ // row number of times
+    	for (int j=0;j<mat1[i].length;j++) { // column number of time
+    		System.out.print(result[i][j] + " ");
+    		result[i][j] = mat1[i][j] + mat2[i][j];
+    	}
+    	System.out.println(""); // Prints a new line
+    }
+*/
+  
+		
+// Wheather a array is sorted or not
+		boolean isSorted = true;
+        int [] arr = {1, 12, 3, 4, 5, 34, 67};
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i] > arr[i+1]){
+                isSorted = false;
+                break;
+            }
+        }
+        if(isSorted){
+            System.out.println("The Array is sorted");
+        }
+        else{
+            System.out.println("The Array is not sorted");
+        }
 	}
 	
 }
