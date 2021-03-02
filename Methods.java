b069bcbcb09d9dcbcdd345a76661e73ca653174b
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-02 23:54:06 +0530

    Access method by creating object

--- Methods.java
+++ Methods.java
@@ -1,3 +1,4 @@
+/*
 import java.util.Scanner;
 public class Methods {
 	static int logic(int x,int y) {
@@ -18,6 +19,34 @@ public class Methods {
 		int b = sc.nextInt();
 		int c;
 		c=  logic (a,b);
+		System.out.println("The value of c = "+c);
+	}
+
+} */
+
+
+// Calling Method using object 
+import java.util.Scanner;
+public class Methods {
+	 int logic(int x,int y) {
+		int z;
+		if(x>y) {
+			z=x+y;	
+		}
+		else {
+			z=(x+y)* 2;
+		}
+		return z;
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of a and b");
+		int a= sc.nextInt();
+		int b = sc.nextInt();
+		int c;
+		Methods obj = new Methods();
+		c= obj. logic (a,b);
 		System.out.println("The value of c = "+c);
 	}
 
