76523016c5059059fcac4e9684a20ec2d3a2b143
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-02 23:43:36 +0530

    Functions

---
+++ Methods.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Methods {
+	static int logic(int x,int y) {
+		int z;
+		if(x>y) {
+			z=x+y;	
+		}
+		else {
+			z=(x+y)* 2;
+		}
+		return z;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of a and b");
+		int a= sc.nextInt();
+		int b = sc.nextInt();
+		int c;
+		c=  logic (a,b);
+		System.out.println("The value of c = "+c);
+	}
+
+}
