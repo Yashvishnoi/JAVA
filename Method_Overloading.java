5be4862573dd57b7f510f5b243b96e7d63bc0326
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-03 00:39:40 +0530

    Passing array to method

--- Method_Overloading.java
+++ Method_Overloading.java
@@ -3,8 +3,13 @@ public class Method_Overloading {
 		System.out.println("I invented a new word !!\n" +"Plagiarism ");
 		
 	}
+	
 	static void Change(int x) {
 		x=92;
+	}
+	
+	static void Change2(int[] arr) {
+		arr[0] = 654;
 	}
 	public static void main(String[] args) {
 		TellJoke();
@@ -13,7 +18,12 @@ public class Method_Overloading {
 		System.out.println("\n\nValue of a before changing "+a);
 		Change(a);
 		System.out.println("Value of a after changing "+a);
-		TellJoke();
+		
+		
+		int[] arr = {10,25,36,85,105};
+		System.out.println("\n\nThe value of array before Changing "+arr[0] );
+		Change2(arr);
+		System.out.println("The value of array after changing "+arr[0]);
 	}
 
 }
