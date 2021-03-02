b2e0dd409c57f7bd3f44ec0a640da98b4132e4fb
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-03 01:24:20 +0530

    Added line 12 and 13

--- Variable_Argument.java
+++ Variable_Argument.java
@@ -9,10 +9,12 @@ public class Variable_Argument {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sum of 2 number " +  Sum(2,5));
-		System.out.println("Sum of 3 number " +  Sum(2,5,7));
-		System.out.println("Sum of 4 number " +  Sum(2,5,7,8));
-		System.out.println("Sum of 5 number " +  Sum(2,5,7,8,9));
+		System.out.println("Sum of 0 number is " + Sum());
+		System.out.println("Sum of 1 number is " + Sum(1));
+		System.out.println("Sum of 2 number is " +  Sum(2,5));
+		System.out.println("Sum of 3 number is " +  Sum(2,5,7));
+		System.out.println("Sum of 4 number is " +  Sum(2,5,7,8));
+		System.out.println("Sum of 5 number is " +  Sum(2,5,7,8,9));
 		
 	}
 
