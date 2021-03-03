3d4ef175786f45daab140ebed98272767c5b39be
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-03 14:34:59 +0530

    Cretead a Class

---
+++ Class_And_Its_Properties.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Employee {
+	int salary;
+	String name;
+	
+	int getSalary() {
+		return salary;
+	}
+	
+	String getName () {
+		return name;
+	}
+	
+	void  setName (String n) {
+		name = n;
+	}
+}
+
+public class Class_And_Its_Properties {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+	}
+
+}
