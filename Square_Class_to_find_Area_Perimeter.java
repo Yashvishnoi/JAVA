79f1fcc6cb62d8fc362f0f7f4760585e822f2364
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-03 15:38:42 +0530

    Added the class and object created

--- Square_Class_to_find_Area_Perimeter.java
+++ Square_Class_to_find_Area_Perimeter.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+
+class Square {
+	int length;
+	
+	void SetValue(int x) {
+		length=x;
+	}
+	
+	int Perimeter () {
+		return 2*length;
+	}
+	
+	int Area() {
+		return 4*length;
+	}
+	
+}
 
 public class Square_Class_to_find_Area_Perimeter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the N Value");
+		int N = sc.nextInt();
+		
+		Square s = new Square();
+		s.SetValue(N);
+		
+		System.out.println("The perimeter of a square is "+ s.Perimeter());
+		System.out.println("The area of a square is "+ s.Area());
+		
 	}
 
 }
