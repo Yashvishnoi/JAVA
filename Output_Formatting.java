324eb831c23f72234fb25c54b5ad43ce3a80e117
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-03 13:24:14 +0530

    Output Formatting

---
+++ Output_Formatting.java
@@ -0,0 +1,36 @@
+/*
+In each line of output there should be two columns:
+The first column contains the String and is left justified using exactly  characters.
+The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+
+Sample Input
+
+java 100
+cpp 65
+python 50
+Sample Output
+
+================================
+java           100 
+cpp            065 
+python         050 
+================================ 
+*/
+
+import java.util.Scanner;
+public class Output_Formatting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("============================");
+		for(int i=0;i<3;i++) {
+			String s = sc.nextLine();
+			int x = sc.nextInt();
+			System.out.printf("%-15s%03%d",s,x);
+		}
+		System.out.println("============================");
+	}
+
+}
+
