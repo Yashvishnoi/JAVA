d7765b758196a51657162333ce74cd6fd6183472
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-04 23:06:14 +0530

    Constructor

--- Constructors.java
+++ Constructors.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
+
+class MyEmployee {
+	private int salary;
+	private String name;
+	
+	public MyEmployee() {
+		System.out.println("This is default Constructor");
+		salary = 15000;
+		name = "Shakal";
+	}
+	
+	public MyEmployee(int s, String n) {
+		System.out.println("Constructor called");
+		salary =s;
+		name=n;
+	}
+	
+	public void Return () {
+		System.out.printf("The name of  a person is %s \n",name);
+		System.out.printf("The salary of a person is %d \n",salary);
+	
+	}
+	
+	
+	
+}
 
 public class Constructors {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		MyEmployee E = new MyEmployee();
+		E.Return();
+		
+		MyEmployee E1 = new MyEmployee(102000,"Mukesh");
+		E1.Return();
 	}
 
 }
---
+++ Guess_the_number_through_class.java
@@ -0,0 +1,12 @@
+class Game {
+	
+}
+
+public class Guess_the_number_through_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
