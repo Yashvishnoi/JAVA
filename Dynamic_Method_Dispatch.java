f7ddea72b00ae5631b1dc94b560da1d49246b24a
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-05 14:53:03 +0530

    Added the objects to access it

--- Dynamic_Method_Dispatch.java
+++ Dynamic_Method_Dispatch.java
@@ -10,6 +10,7 @@ class Phone {
 }
 
 class SmartPhone extends Phone {
+	@Override
 	void Music() {
 		System.out.println("Music through Jio Savaan...");
 	}
@@ -22,6 +23,11 @@ public class Dynamic_Method_Dispatch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Phone P = new SmartPhone() ;
+		P.Calling();
+		P.Music(); // This invokes Music of Derived Class
+	//	P.Game(); This we can't access 
+		
 	}
 
 }
