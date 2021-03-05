9f96935ce5f00670d6a0ad0b41b7a5ab0e12b634
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-05 14:33:55 +0530

    Phone and SmartPhone Class Created

---
+++ Dynamic_Method_Dispatch.java
@@ -0,0 +1,27 @@
+class Phone {
+	void Calling () {
+		System.out.println("We can call...");
+	}
+	
+	void Music () {
+		System.out.println("Music can play in any phone...");
+	}
+	
+}
+
+class SmartPhone extends Phone {
+	void Music() {
+		System.out.println("Music through Jio Savaan...");
+	}
+	
+	void Game() {
+		System.out.println("We can play PUBG");
+	}
+}
+public class Dynamic_Method_Dispatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
