9360af7f96fded90c25e1ba2ea73ef9a97d950c9
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-07 23:47:14 +0530

    Class added

--- Interfaces.java
+++ Interfaces.java
@@ -1,3 +1,21 @@
+interface Bicycle {
+	void ApplyBreak(int decrement);
+	void SpeedUP(int increment);
+}
+
+class AvonCycle implements Bicycle {
+	void blowhorn() { 
+		System.out.println("Pee Poo Pee Poo :)");
+	}
+	
+	public void ApplyBreak(int decrement) {
+		System.out.println("Break Applied");
+	}
+	
+	public void SpeedUP(int increment) {
+		System.out.println("Speed UP");
+	}
+}
 
 public class Interfaces {
 
