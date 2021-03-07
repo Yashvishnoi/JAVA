af4ea88c9f202abd63e7ba87997cd48fb66d7240
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-08 00:21:48 +0530

    Added the main function

--- Interfaces.java
+++ Interfaces.java
@@ -21,13 +21,26 @@ class AvonCycle implements Bicycle,GearBox {
 	public void SpeedUP(int increment) {
 		System.out.println("Speed UP");
 	}
+	
+	public void Gearup() {
+		System.out.println("Increase the gear");
+	}
+	
+	public void GearDown() {
+		System.out.println("Gear Down");
+	}
 }
 
 public class Interfaces {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		AvonCycle AC = new AvonCycle();
+		AC.ApplyBreak(1);
+		AC.SpeedUP(2);
+		AC.Gearup();
+		AC.GearDown();
+		
 	}
 
 }
