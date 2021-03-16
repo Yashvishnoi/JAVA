bff47ce5fd2285b406756ea634a6c39a67d3642e
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-16 23:33:37 +0530

    Added the Classes

---
+++ PolymorphismWithDataMember.java
@@ -0,0 +1,25 @@
+//A method is overridden, not the data members, so runtime polymorphism can't be achieved by data members.
+
+class Bike{
+	int SpeedLimit = 80;
+}
+
+class Splender extends Bike {
+	int SpeedLimit = 100;
+}
+
+class GSA1200 extends Bike{
+	int SpeedLimit = 200;
+}
+
+class Ninja1000 extends Bike {
+	int SpeedLimit = 300;
+}
+public class PolymorphismWithDataMember {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
