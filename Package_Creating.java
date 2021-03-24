bd57930e5fb3aa9ea9a0c32d552b27cbb606ae93
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-24 23:37:58 +0530

    Added the util package

---
+++ Package_Creating.java
@@ -0,0 +1,11 @@
+import java.util.*; // All classes inside it to be imported
+
+public class Package_Creating {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+		
+		System.out.println("This is my Scanner ");
+	}
+
+}
