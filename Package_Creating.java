a52a4fc3348fc23c9175f1bcaa995d03f714a802
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-24 23:39:46 +0530

    give the path of package

--- Package_Creating.java
+++ Package_Creating.java
@@ -1,10 +1,10 @@
-import java.util.*; // All classes inside it to be imported
+// import java.util.*; // All classes inside it to be imported
 
 public class Package_Creating {
 
 	public static void main(String[] args) {
-	Scanner sc = new Scanner (System.in);
-		
+//		Scanner sc = new Scanner (System.in);
+	java.util.Scanner sc = new java.util.Scanner(System.in);
 		System.out.println("This is my Scanner ");
 	}
 
