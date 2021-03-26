740d10942fff63e2b6fb316b8baba3704a731149
Yash Vishnoi <jivishnoi@gmail.com>
2021-03-26 12:41:40 +0530

    Added the Unchecked exception

--- Checked_and_Unchecked_Exception.java
+++ Checked_and_Unchecked_Exception.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 public class Checked_and_Unchecked_Exception {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter value of a");
+		int a=sc.nextInt();
+	
+		System.out.println("Enter value of b");
+		int b= sc.nextInt();
+		System.out.println("Entered value of a and b is "+a+" & "+b);
+		
+		
+		int c= a/b;
+		System.out.println("Divison of a and b is "+c);
 	}
-
 }
