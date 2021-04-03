8d7f944ab26d51dd5c3b5c9e4cf854ed9e881cc4
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-03 15:07:25 +0530

    We acessed through world only public access modifier

--- PackagesDone/Importing.java
+++ PackagesDone/Importing.java
@@ -3,7 +3,12 @@ import codewithharry.Gym.harryGym;
 public class Importing{
     public static void main(String[] args) {
         System.out.println("This is importing");
-        harryGym g = new harryGym();
-        g.hey();
+        harryGym c = new harryGym();
+        System.out.println(c.x);
+        // We can't access the protected default and Private members
+		// System.out.println(c.y);
+		// System.out.println(c.z);
+		//System.out.println(c.a);
+        c.hey();
     }
 }
--- PackagesDone/harryGym.java
+++ PackagesDone/harryGym.java
@@ -1,10 +1,20 @@
 package codewithharry. Gym;
 
 public class harryGym {
+    public int x=5;
+	protected int y=45;
+	int z=6;
+	private int a = 38;
+	
+	public void meth1() {
+		System.out.println(x); // Uses public modifier in same class
+		System.out.println(y);// Uses protected modifier in same class
+		System.out.println(z);// Uses default modifier in same class
+		System.out.println(a); // Uses private modifier in same class
+	}
     public void hey() {
         System.out.println("Hello");
     } 
-
     public static void main(String[] args) {
         System.out.println("This is harry Gym");
     }
