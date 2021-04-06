2af944e78c1ad002e037e8b3a42b5be51f0e18b3
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-06 23:58:36 +0530

    Added the [B2nd implemeted class

--- Threading2.java
+++ Threading2.java
@@ -1,4 +1,16 @@
 
+class MyThread11 implements Runnable{
+	public void run() {
+		System.out.println("I am Thread using Runnable class");
+	}
+}
+
+class MyThread22 implements Runnable{
+	public void run() {
+		System.out.println("Runnable class");
+	}
+}
+
 public class Threading2 {
 
 	public static void main(String[] args) {
