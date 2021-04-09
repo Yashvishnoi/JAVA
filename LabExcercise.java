3b5f75e8be1558b46d168834840b52d35571663a
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-09 12:52:46 +0530

    Done lab experiment

---
+++ LabExcercise.java
@@ -0,0 +1,9 @@
+
+public class LabExcercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LabReport9.java
@@ -0,0 +1,47 @@
+class LowBalance extends Exception
+{
+   LowBalance(String s){ 
+       super(s); 
+      } 
+   }
+
+
+class Account {
+	int AccId;
+	double Balance;
+	
+	public Account(int AccId1 , double Balance1) {
+		AccId=AccId1;
+		this.Balance=Balance1;
+	}
+	
+	double deposit(double amount) {
+		Balance=+amount;
+		return Balance;
+	}
+	
+	double withdraw(double amount) throws LowBalance {
+		if(amount<=Balance) {
+			Balance=-amount;
+		}
+		else {
+			throw new LowBalance("Insufficient Amount");
+		}
+		return Balance;
+}
+}
+public class LabReport9 {
+
+	public static void main(String[] args) {
+		Account obj =new Account(11034,10000);
+		try 
+	      {
+	         System.out.println(obj.withdraw(100));
+	      }
+	      catch (LowBalance e)
+	      {
+	         e.printStackTrace();
+	      }
+		
+	}
+}
