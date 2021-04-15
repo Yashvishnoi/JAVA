a91acbfa64fe7804df96200bdf6871c46168be8d
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-16 00:04:25 +0530

    Added the double name to thread

--- ThreadConstructor.java
+++ ThreadConstructor.java
@@ -12,9 +12,15 @@ class MyThr extends Thread{
 public class ThreadConstructor {
 
 	public static void main(String[] args) {
-		MyThr t = new MyThr("Harry");
-		t.start();
-		System.out.println("The id of the thread t is "+t.getId());
-		System.out.println("The name of the thread t is "+t.getName());
+		MyThr t1= new MyThr("Harry");
+		MyThr t2 = new MyThr("Bawa");
+
+		t1.start();
+		t2.start();
+		System.out.println("The id of the thread t1 is "+t1.getId());
+		System.out.println("The name of the thread t1 is "+t1.getName());
+		
+		System.out.println("The id of the thread t2 is "+t2.getId());
+		System.out.println("The name of the thread t2 is "+t2.getName());
 	}
 }
