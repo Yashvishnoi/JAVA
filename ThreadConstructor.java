513cdc321ddbf569e755e7714d5e57ff9b171bb7
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-15 23:55:39 +0530

    Added the thread class

--- ThreadConstructor.java
+++ ThreadConstructor.java
@@ -1,4 +1,13 @@
-
+class MyThr extends Thread{
+	public MyThr(String name) {
+		super(name);
+	}
+	public void run() {
+		while(true) {
+			System.out.println("I am a thread");
+		}
+	}
+}
 public class ThreadConstructor {
 
 	public static void main(String[] args) {
