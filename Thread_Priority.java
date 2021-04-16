d803fa7ab33cdcb7f9b87396475dfad93f2236ac
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-17 00:48:08 +0530

    Added the thread class

--- Thread_Priority.java
+++ Thread_Priority.java
@@ -1,9 +1,15 @@
+class MyThr1 extends Thread {
+	public MyThr1(String Name) {
+		super(Name);
+	}
+	public void run() {
+		int i=34;
+		System.out.println("Thank You");
+	}
+}
 
 public class Thread_Priority {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
 	}
-
 }
