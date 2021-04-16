b939b5d8b3ce5b52f2128409a4e3c083b9c4cc89
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-17 00:52:45 +0530

    Made the objects of class

--- Thread_Priority.java
+++ Thread_Priority.java
@@ -10,6 +10,12 @@ class MyThr1 extends Thread {
 
 public class Thread_Priority {
 	public static void main(String[] args) {
-	
+		MyThr1 t1 = new MyThr1("Harry");
+		MyThr1 t2 = new MyThr1("Danny");
+		MyThr1 t3 = new MyThr1("Harry");
+		MyThr1 t4 = new MyThr1("Harry");
+		MyThr1 t5 = new MyThr1();
+		
+		
 	}
 }
