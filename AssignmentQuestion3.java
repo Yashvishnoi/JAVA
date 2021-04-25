2b64c6662423c18fff2169b614f79fdd14803f80
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-25 19:20:29 +0530

    Added the code

---
+++ AssignmentQuestion3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class AssignmentQuestion3
+{
+    
+    public static StringBuffer encrypt(String text, int s)
+    {
+        StringBuffer result= new StringBuffer();
+ 
+        for (int i=0; i<text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                                  s - 65) % 26 + 65);
+                result.append(ch);
+            }
+            else
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                                  s - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+ 
+    
+    public static void main(String[] args)
+    {
+    	Scanner sc= new Scanner(System.in);
+    	
+    	System.out.println("Enter the String");
+        String text = sc.nextLine();
+        System.out.println("Enter the Enccoding Value");
+        int s = sc.nextInt();
+        System.out.println("Output: " + encrypt(text, s));
+    }
+}
---
+++ AssignmentQuestion5.java
@@ -0,0 +1,38 @@
+class MyThread implements Runnable {
+String name;
+Thread t;
+    MyThread (String thread){
+    name = thread; 
+    t = new Thread(this, name);
+System.out.println("New thread: " + t);
+t.start();
+}
+ 
+ 
+public void run() {
+ try {
+     for(int i = 100; i > 0; i--) {
+     System.out.println(name);
+      Thread.sleep(1000);
+}
+}catch (InterruptedException e) {
+     System.out.println(name + "Interrupted");
+}
+     System.out.println(name + " exiting.");
+}
+}
+ 
+class AssignmentQuestion5 {
+public static void main(String args[]) {
+     new MyThread("I");
+     new MyThread("Love");
+     new MyThread("UPES");
+try {
+     Thread.sleep(10000);
+} catch (InterruptedException e) {
+      System.out.println("Main thread Interrupted");
+}
+      System.out.println("Main thread exiting.");
+      }
+}
+ 
