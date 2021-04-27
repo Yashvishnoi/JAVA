fb716248d0ff78a102164fa52041d3777fad7776
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-27 23:48:32 +0530

    Added the thread class

--- Java_Thread_Method.java
+++ Java_Thread_Method.java
@@ -1,9 +1,25 @@
 
-public class Java_Thread_Method {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class ThreadMethod1 extends Thread{
+	public void run() {
+		while(true) {
+			System.out.println("Thank You 1111");
+		}
 	}
+}
 
+class ThreadMethod2 extends Thread{
+	
+	public void run() {
+		while(true) {
+			System.out.println("Thank You 2222");
+		}
+	}
+}
+public class Java_Thread_Method {
+	public static void main(String[] args) {
+		ThreadMethod1 t1 = new ThreadMethod1();
+		ThreadMethod1 t2 = new ThreadMethod1();
+		t1.start();
+		t2.start();
+	}
 }
