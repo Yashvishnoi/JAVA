4358a00d4ccd682db46681b4306ea4f6682635db
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-29 14:36:47 +0530

    Added the first file for JDBC

---
+++ JDBC_Practice.java
@@ -0,0 +1,15 @@
+import java.sql.*;  
+class JDBC_Practice{  
+public static void main(String args[]){  
+try{  
+Class.forName("com.mysql.jdbc.Driver");  
+Connection con=DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/JDBC","root","Yash@123");  
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from shop;");  
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+con.close();  
+}catch(Exception e){ System.out.println(e);}  
+}  
+}
