bbaebb695a0deaf688e8ef165ef3e874d9f135a3
Yash Vishnoi <jivishnoi@gmail.com>
2021-05-01 01:01:47 +0530

    Final

---
+++ Final_Keyword.java
@@ -0,0 +1,10 @@
+class Final_Keyword{
+  final int speedlimit=90;//final variable
+  void run(){
+   int speedlimit=400;
+  }
+  public static void main(String args[]){
+  Final_Keyword obj=new  Final_Keyword();
+  obj.run();
+  }
+ }
