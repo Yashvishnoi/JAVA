957d5c83c9cfa81572c3c4140417cb9fd140aacf
Yash Vishnoi <jivishnoi@gmail.com>
2021-04-30 22:34:45 +0530

    Table of a number

---
+++ Print_Table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Print_Table {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a = sc.nextInt();
+		
+		for(int i=1;i<=10;i++) {
+			System.out.println(a+" * "+i +"="+a*i);
+		}
+	}
+}
