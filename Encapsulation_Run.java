26539bae3eae6b55825badc99f8120424e83e410
Yash Vishnoi <jivishnoi@gmail.com>
2021-05-04 22:24:04 +0530

    Added the Method

--- Encapsulation_Run.java
+++ Encapsulation_Run.java
@@ -1,9 +1,12 @@
 
 public class Encapsulation_Run {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	   public static void main(String args[]) {
+	      Encapsulation encap = new Encapsulation();
+	      encap.setName("Vijay");
+	      encap.setAge(25);
+	      encap.setIdNum("500077690");
 
+	      System.out.print("Name : " + encap.getName() + " Age : " + encap.getAge());
+	   }
 	}
-
-}
