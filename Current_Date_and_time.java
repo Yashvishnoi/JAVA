7f005c93eec3d712fe4f1918102722eea77a77a9
Yash Vishnoi <jivishnoi@gmail.com>
2021-05-06 00:34:10 +0530

    Added the code

--- Current_Date_and_time.java
+++ Current_Date_and_time.java
@@ -1,9 +1,10 @@
 
-public class Current_Date_and_time {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+public class Current_Date_and_time {    
+  public static void main(String[] args) {    
+   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+   LocalDateTime now = LocalDateTime.now();  
+   System.out.println(dtf.format(now));  
+  }    
+}    
